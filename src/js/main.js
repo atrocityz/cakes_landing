1f7f52c253e165c6d1a2608abe89a6bbec28d326
atrocityz <disbalance17@Mail.ru>
2025-01-24 23:31:18 +0700

    fix: переделал облака для секций & поправил некоторые элементы

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,13 @@
-import Swiper from "swiper";
-import { Navigation } from "swiper/modules";
+import Swiper from 'swiper'
+import { Navigation } from 'swiper/modules'
 
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper('.swiper', {
   modules: [Navigation],
-  spaceBetween: -100,
   slidesPerView: 1,
   centeredSlides: true,
   initialSlide: 1,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
-});
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
